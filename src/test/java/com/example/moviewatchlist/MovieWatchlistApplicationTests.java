7548bddd2fa9f38f839269f262b0022b24bcd88e
prof-xed <mr.j.xed+git@gmail.com>
2025-06-06 20:42:40 +0200

    [init] Sprint Boot project setup (Web, JPA, MySQL, and Lombok)

---
+++ src/test/java/com/example/moviewatchlist/MovieWatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviewatchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
